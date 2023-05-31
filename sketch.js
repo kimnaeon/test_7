625ca5b3e90d5c9c29cbeb999ecc4de71d684284
kimnaeon <120731320+kimnaeon@users.noreply.github.com>
2023-06-01 00:45:41 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function setup() {
 }
 
 function draw() {
-  background(rotationX,rotationY,rotationZ);
+  background(xMotion, yMotion, zMotion);
   fill(255)
 
   // we can use rotationZ, rotationX and rotationY
